d6bf936853e6cf7bf0d008c06d646a0b1848c37d
Tran Truong <chantruong753@gmail.com>
2024-05-22 20:23:31 +0700

    create layout with  handlebars

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({ extended: true }));
+
 app.engine('hbs', engine ({
   extname: '.hbs' // đổi đuôi handlebars => hbs
 }));
@@ -26,7 +28,19 @@ app.set("views", path.resolve(__dirname, "./resources/views"));
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.render("home");
+  res.render("home",{
+    title: "HOME",
+    mess: "hello world",
+  });
+})
+
+
+app.get('/search', (req, res) => {
+  
+  console.log(req.query.q);
+  res.render("search",{
+    query:req.query.q,
+  });
 })
 
 app.listen(port, () => {
